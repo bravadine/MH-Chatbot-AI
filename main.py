97b3ac7f0ceca26afb4ed0b71767e1d95a437757
I Nyoman Yogasmara <ygsmr.public@protonmail.com>
2022-12-20 19:58:39 +0700

    Updated interface to check for bot context

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from bot import Bot
 
 if __name__ == "__main__":
     bot = Bot()
-    print("Chatbot is ready to talk with you! Type \"quit\" to end the session.")
-    while True:
+    print("Chatbot is ready to talk with you! Say goodbye to the bot to end the session.")
+    while bot.get_current_context() != "exit":
         message = input("You:")
-        if message.lower() == "quit":
-            break
         response = bot.get_bot_response(message)
         print("Bot:", response)
