bd370c92bd09082e13630e106b1454ae3eec05b8
I Nyoman Yogasmara <ygsmr.public@protonmail.com>
2022-12-20 19:09:45 +0700

    Added option to end program

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from bot import Bot
 
 if __name__ == "__main__":
     bot = Bot()
+    print("Chatbot is ready to talk with you! Type \"quit\" to end the session.")
     while True:
-        message = input("You : ")
+        message = input("You:")
+        if message.lower() == "quit":
+            break
         response = bot.get_bot_response(message)
-        print("Bot :",response)
+        print("Bot:", response)
